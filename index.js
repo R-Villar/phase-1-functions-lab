2489d368138749bf8a65c68e9bb5342b22d93c68
Remberto Villar <rembertovillar@gmail.com>
2022-06-28 09:59:21 -0400

    commit tree d8d88bfde1aea34c985be1bf41ee1fe81b04fd60

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(42 - pickupLocation)
+}
+
+function distanceFromHqInFeet(someValue) {
+    return 264 * distanceFromHqInBlocks(someValue)
+}
+
+function distanceTravelledInFeet(start, destination) {
+    //returns the number of feet traveled
+    return Math.abs(start - destination) * 264;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+    const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+  }
